ef0d958f1be54fa26ecbd57b865cb961740fd571
KosarS <kosarsohani83@gmail.com>
2025-01-15 10:42:33 +0330

    Add other changes

---
+++ src/templatetags/words_tags.py
@@ -0,0 +1,18 @@
+# templatetags/words_tags.py
+
+from django import template
+from group3.models import BOXES, Word
+
+register = template.Library()
+
+@register.inclusion_tag("group3/box_links.html")
+def boxes_as_links():
+    boxes = []
+    for box_num in BOXES:
+        word_count = Word.objects.filter(box=box_num).count()
+        boxes.append({
+            "number": box_num,
+            "word_count": word_count,
+        })
+
+    return {"boxes": boxes}
